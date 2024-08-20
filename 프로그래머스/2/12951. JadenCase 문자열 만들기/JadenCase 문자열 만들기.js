bf7fa0907094a211fc5b1884b311eab0fb84ffef
Sunggeun Lee <121940314+LeeSungGeun7@users.noreply.github.com>
2024-08-20 17:19:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,10 +1,16 @@
-function solution(s) {
-    ans = []
-    s.split(' ').forEach((v,idx)=>{
-        let i = v.substring(0,1).toUpperCase()
-        let c = v.substring(1,v.length).toLowerCase()
-        ans.push(i + c) 
-    })
+// function solution(s) {
+//     ans = []
+//     s.split(' ').forEach((v,idx)=>{
+//         let i = v.substring(0,1).toUpperCase()
+//         let c = v.substring(1,v.length).toLowerCase()
+//         ans.push(i + c) 
+//     })
     
-    return ans.join(' ');
+//     return ans.join(' ');
+// }
+
+function solution(s) {
+    return s.split(' ')
+        .map(v => v.charAt(0).toUpperCase() + v.slice(1).toLowerCase())
+        .join(' ');
 }
