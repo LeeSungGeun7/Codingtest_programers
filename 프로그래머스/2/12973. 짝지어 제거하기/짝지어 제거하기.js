a023977a4181b528c6fdf7fff1c2fbfe4e93b62e
Sunggeun Lee <121940314+LeeSungGeun7@users.noreply.github.com>
2024-08-23 14:04:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 52.33 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let stack = []
+    
+    for ( let i of s ) {
+        if ( stack[stack.length-1] === i ) {
+            stack.pop()
+        } else {
+            stack.push(i)
+        }
+    }
+    return stack.length > 0 ? 0 : 1
+}
