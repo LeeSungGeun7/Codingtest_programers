f5e6cf6449399880a3241c22f2edc9b95753fe88
Sunggeun Lee <121940314+LeeSungGeun7@users.noreply.github.com>
2024-08-29 14:04:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    if (n <= 2) return n;
+    
+    let arr = [1,1]
+    
+    for (let i = 2 ; i <= n ; i++) {
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567
+    }
+
+    return arr[n] 
+    
+    
+}
